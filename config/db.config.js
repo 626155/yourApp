b2fd49a3203ad29d907a91783086abf46bec5863
DELL <pravesh@ubitechsolutions.com>
2023-05-01 03:03:14 +0530

    datebase credential changes

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-  HOST: process.env.DB_HOST || 'localhost',
-  USER: process.env.DB_USER || 'root',
-  PASSWORD: process.env.DB_PASSWORD || '',
+  HOST: process.env.DB_HOST || 'db4free.net',
+  USER: process.env.DB_USER || 'yourapp',
+  PASSWORD: process.env.DB_PASSWORD || 'admin@123',
   DB: process.env.DB_NAME || 'userdb',
   PORT: process.env.DB_PORT || '3306',
   dialect: 'mysql'
